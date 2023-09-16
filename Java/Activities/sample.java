9bce974113804078cd2d79908088d0091dec0068
RohitNaikwadi <143474372+RohitNaikwadi@users.noreply.github.com>
2023-09-16 13:33:31 +0530

    Add files via upload

---
+++ Java/Activities/sample.java
@@ -0,0 +1,18 @@
+package Examples;
+
+public class sample {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        int a=1;
+        int b=4;
+        System.out.println(a+b);
+       // string str1="asd";
+        String str2="af";
+        System.out.println(str2);
+
+    }
+
+
+}
